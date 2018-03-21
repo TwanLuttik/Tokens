925aaf2b52edb30b60fd761d49a20e9c5371dd12
Twan Luttik <twanluttik@gmail.com>
2018-03-21 22:43:07 +0100

    added vault support

--- src/main/java/com.twanl.tokens/Tokens.java
+++ src/main/java/com.twanl.tokens/Tokens.java
@@ -1,51 +1,65 @@
 package com.twanl.tokens;
 
 import com.twanl.tokens.events.JoinEvent;
-import com.twanl.tokens.items.CustomItems;
+import com.twanl.tokens.items.TokenItem;
 import com.twanl.tokens.utils.ConfigManager;
 import com.twanl.tokens.utils.Metrics;
 import com.twanl.tokens.utils.Strings;
 import com.twanl.tokens.utils.UpdateChecker;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 
 public class Tokens extends JavaPlugin {
 
+    //TODO: SQL Support
+    //TODO: Vault Support
+    //TODO: Getting the 2 commands working
+    //TODO: Change the reload command for better checking for file or path or someting.
+    //TODO: API Support so thath other Dev can use this Plugin
 
+
+    private static final Logger log = Logger.getLogger("Minecraft");
     protected PluginDescriptionFile pdfFile = getDescription();
     private final String PluginVersionOn = ChatColor.GREEN + "(" + pdfFile.getVersion() + ")";
     private final String PluginVersionOff = ChatColor.RED + "(" + pdfFile.getVersion() + ")";
     private UpdateChecker checker;
+
     public ConfigManager cfgM;
+    //public TokensAPI tokensApi;
 
 
 
     public void onEnable() {
+        //tokensApi = new TokensAPI();
         Metrics metrics = new Metrics(this);
 
+        if (getServer().getPluginManager().getPlugin("Vault") != null) {
+            getServer().getConsoleSender().sendMessage(Strings.green + Strings.logName + "Detected Vault.");
+            setupEconomy();
+        } else {
+            getServer().getConsoleSender().sendMessage(Strings.green + Strings.logName + Strings.red + "VAULT NOT DETECTED, Some commands won't work.");
+        }
 
-        /*
-        // check if players folder exist else creating a new one
-        playerFolder = new File(getDataFolder(), "players");
-        if (!playerFolder.exists()) {
-            getServer().getConsoleSender().sendMessage(Strings.red + "players folder not found, creating a new one\n ");
-            try {
-                playerFolder.mkdir();
-                getServer().getConsoleSender().sendMessage(Strings.green + "players folder created");
-            } catch (Exception e) {
-                getServer().getConsoleSender().sendMessage(Strings.red + "Failed to create players folder");
-                e.printStackTrace();
-            }
+
+/*
+        if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            //getServer().getPluginManager().disablePlugin(this);
+            return;
         }
         */
 
 
 
-        checker = new UpdateChecker(this);
 
+        checker = new UpdateChecker(this);
         if (checker.isConnected()) {
             if (checker.hasUpdate()) {
                 getServer().getConsoleSender().sendMessage(Strings.green + "");
@@ -79,7 +93,7 @@ public class Tokens extends JavaPlugin {
     public void Load() {
         // Register listeners
         getServer().getPluginManager().registerEvents(new JoinEvent(), this);
-        getServer().getPluginManager().registerEvents(new CustomItems(), this);
+        getServer().getPluginManager().registerEvents(new TokenItem(), this);
 
         // Register Command Class
         Commands commands = new Commands();
@@ -96,6 +110,20 @@ public class Tokens extends JavaPlugin {
         cfgM.setup();
         cfgM.savePlayers();
         cfgM.reloadplayers();
+    }
+
+
+
+
+
+    public static Economy economy;
+
+    private boolean setupEconomy() {
+        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(Economy.class);
+        if (economyProvider != null) {
+            economy = (Economy)economyProvider.getProvider();
+        }
+        return economy != null;
     }
 
 
