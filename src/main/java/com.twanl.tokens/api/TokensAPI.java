1e54116086cd123d2b8dc2f757c87c1b2d3a1049
Twan Luttik <twanluttik@gmail.com>
2018-03-21 22:44:04 +0100

    added new methode in the api class

--- src/main/java/com.twanl.tokens/api/TokensAPI.java
+++ src/main/java/com.twanl.tokens/api/TokensAPI.java
@@ -4,12 +4,11 @@ import com.twanl.tokens.Functions;
 import com.twanl.tokens.Tokens;
 import com.twanl.tokens.utils.ConfigManager;
 import com.twanl.tokens.utils.Strings;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import sun.security.krb5.Config;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.UUID;
 
 
@@ -18,6 +17,7 @@ public class TokensAPI {
     private static Tokens plugin = Tokens.getPlugin(Tokens.class);
     private ConfigManager cfgM = new ConfigManager();
     private Functions F = new Functions();
+
 
 
 
@@ -63,6 +63,114 @@ public class TokensAPI {
         cfgM.savePlayers();
     }
 
+/*
+    public void balance(UUID uuid, Player p) {
+        String balanceTokens = String.valueOf(cfgM.getPlayers().getInt(uuid + ".tokens"));
+        p.sendMessage(Strings.green + "You have " + balanceTokens + " Tokens");
+    }
+
+
+    public void hasAcount (UUID uuid, Player p) {
+
+        if (!cfgM.getPlayers().contains(uuid.toString())) {
+
+        }
+
+    }
+
+*/
+
+
+    /**
+     *
+     * Example:
+     *
+     * 1 Tokens costs = 100 money
+     * /tokens buy <amount_of_tokens>
+     *
+     *
+     *
+     *
+     * 1 Tokens is worth = 90 Money
+     * /tokens sell <amount_of_tokens>
+     *
+     */
+
+
+    private Economy economy = Tokens.economy;
+
+    @SuppressWarnings("deprecation")
+    public boolean convertToTokens (UUID uuid, int amount, Player p) {
+
+        // getting some information
+        int tokensValue = plugin.getConfig().getInt("tokens.buy_price");
+        int totalCheckOut = amount * tokensValue;
+        Player buyer = p.getPlayer();
+        int playerTokens = cfgM.getPlayers().getInt(uuid + ".tokens");
+
+        // check if the player has enough balance to continue the transaction
+        if (totalCheckOut > economy.getBalance(buyer)) {
+            p.sendMessage(Strings.red + "You don't have enough money to buy Tokens!\n" +
+            " \n" +
+            Strings.gold + "Total costs: $" + totalCheckOut);
+            return true;
+        }
+
+
+        // ....
+        EconomyResponse r = economy.withdrawPlayer(buyer, totalCheckOut);
+        if (r.transactionSuccess()) {
+
+
+            cfgM.getPlayers().set(uuid + ".tokens", playerTokens + amount);
+            cfgM.savePlayers();
+            p.sendMessage(Strings.green + "You bought " + amount + " Tokens for " + totalCheckOut);
+
+            return true;
+        } else {
+            p.sendMessage("An error accured!");
+            return true;
+        }
+    }
+
+    public boolean
+    convertToMoney (UUID uuid, int amount, Player p) {
+
+        // getting some information
+        int tokensValue = plugin.getConfig().getInt("tokens.sell_price");
+        int playerTokens = cfgM.getPlayers().getInt(uuid + ".tokens");
+        int totalCheckOut = amount * tokensValue;
+        Player buyer = p.getPlayer();
+
+
+        // Check if player has enough tokens
+        if (amount > playerTokens) {
+            p.sendMessage(Strings.red + "You don't have that many tokens to sell!\n" +
+                    " \n" +
+                    Strings.gold + "Total Tokens: " + playerTokens);
+            return true;
+        }
+
+
+        // ....
+        EconomyResponse r = economy.depositPlayer(buyer, totalCheckOut);
+        if (r.transactionSuccess()) {
+
+            cfgM.getPlayers().set(uuid + ".tokens", playerTokens - amount);
+            cfgM.savePlayers();
+            p.sendMessage(Strings.green + "You sold " + amount + " Tokens for " + totalCheckOut);
+
+            return true;
+        } else {
+            p.sendMessage("An error accured!");
+            return true;
+        }
+
+    }
+
+
+
+
 
 
     // Check if player exist in file
@@ -74,6 +182,8 @@ public class TokensAPI {
         }
         return false;
     }
+
+
 
 
 
