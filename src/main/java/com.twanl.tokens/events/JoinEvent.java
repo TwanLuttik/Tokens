acffd61c9e0b620cfa8faff355a29b9df04dc6ba
Twan Luttik <twanluttik@gmail.com>
2018-03-22 01:32:56 +0100

    addad hasAccount methode

--- src/main/java/com.twanl.tokens/events/JoinEvent.java
+++ src/main/java/com.twanl.tokens/events/JoinEvent.java
@@ -1,6 +1,7 @@
 package com.twanl.tokens.events;
 
 import com.twanl.tokens.Tokens;
+import com.twanl.tokens.api.TokensAPI;
 import com.twanl.tokens.utils.Strings;
 import com.twanl.tokens.utils.UpdateChecker;
 import net.minecraft.server.v1_12_R1.IChatBaseComponent;
@@ -19,6 +20,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinEvent implements Listener {
 
     private Tokens plugin = Tokens.getPlugin(Tokens.class);
+    private TokensAPI tokenApi = new TokensAPI();
 
 
     @EventHandler
@@ -26,6 +28,7 @@ public class JoinEvent implements Listener {
         Player p = e.getPlayer();
 
 
+        // Updata message
         if (plugin.getConfig().getBoolean("update_message")) {
             if (p.hasPermission("tokens.update")) {
                 final Player p1 = e.getPlayer();
@@ -57,5 +60,8 @@ public class JoinEvent implements Listener {
 
             }
         }
+
+
+        tokenApi.hasAccount(p.getUniqueId());
     }
 }
