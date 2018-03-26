17db08e5b16e6d74998dca146d2daa90bce07564
Twan Luttik <twanluttik@gmail.com>
2018-03-26 14:22:52 +0200

    added a NMS

---
+++ src/main/java/com.twanl.tokens/NMS/VersionHandler.java
@@ -0,0 +1,20 @@
+package com.twanl.tokens.NMS;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Twan on 3/26/2018.
+ **/
+public abstract interface VersionHandler {
+
+    public abstract void sendActionBar(Player paramPlayer, String paramString);
+
+    public abstract void sendClickableMessage(Player paramPlayer, String paramString1, String paramString2, String paramString3);
+
+    public abstract void sendHoverableMessage(Player paramPlayer, String paramString1, String paramString2, String paramString3);
+
+    public abstract void sendClickableHoverableMessage(Player paramPlayer, String textpart, String clickabletext, String hovertext, String runcommand);
+
+    public abstract void sendClickableHovarableMessageURL(Player p, String clickabletext, String hovertext, String URL);
+
+}
