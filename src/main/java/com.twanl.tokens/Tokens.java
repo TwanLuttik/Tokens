496483a51556bad25647bfcc3ab943aaaad8ec0e
Twan Luttik <twanluttik@gmail.com>
2018-03-26 14:23:56 +0200

    added a checker for supported versions

--- src/main/java/com.twanl.tokens/Tokens.java
+++ src/main/java/com.twanl.tokens/Tokens.java
@@ -1,5 +1,14 @@
 package com.twanl.tokens;
 
+import com.twanl.tokens.NMS.VersionHandler;
+import com.twanl.tokens.NMS.v1_12.v1_12_R1;
+import com.twanl.tokens.NMS.v1_11.v1_11_R1;
+import com.twanl.tokens.NMS.v1_10.v1_10_R1;
+import com.twanl.tokens.NMS.v1_9.v1_9_R2;
+import com.twanl.tokens.NMS.v1_9.v1_9_R1;
+import com.twanl.tokens.NMS.v1_8.v1_8_R3;
+import com.twanl.tokens.NMS.v1_8.v1_8_R2;
+import com.twanl.tokens.NMS.v1_8.v1_8_R1;
 import com.twanl.tokens.api.TokensAPI;
 import com.twanl.tokens.commands.Commands;
 import com.twanl.tokens.events.JoinEvent;
@@ -13,6 +22,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.awt.*;
+import java.util.logging.Level;
 
 /**
  * Created by Twan on 3/22/2018.
@@ -32,6 +44,7 @@ public class Tokens extends JavaPlugin {
     private final String PluginVersionOn = Strings.green + "(" + pdfFile.getVersion() + ")";
     private final String PluginVersionOff = Strings.red + "(" + pdfFile.getVersion() + ")";
     private UpdateChecker checker;
+    public VersionHandler nms;
 
     public static Economy economy;
     public ConfigManager cfgM;
@@ -40,8 +53,10 @@ public class Tokens extends JavaPlugin {
 
 
 
-
     public void onEnable() {
+
+
+        getServerVersion();
         // Api for other DEV
         tokensApi = new TokensAPI();
         Metrics metrics = new Metrics(this);
@@ -115,5 +130,31 @@ public class Tokens extends JavaPlugin {
         return economy != null;
     }
 
+
+    private void getServerVersion() {
+        String a = getServer().getClass().getPackage().getName();
+        String version = a.substring(a.lastIndexOf('.') + 1);
+
+        // Check
+        if (version.equalsIgnoreCase("v1_8_R1")) {
+            nms = new v1_8_R1();
+        } else if (version.equalsIgnoreCase("v1_8_R2")) {
+            nms = new v1_8_R2();
+        } else if (version.equalsIgnoreCase("v1_8_R3")) {
+            nms = new v1_8_R3();
+        } else if (version.equalsIgnoreCase("v1_9_R1")) {
+            nms = new v1_9_R1();
+        } else if (version.equalsIgnoreCase("v1_9_R2")) {
+            nms = new v1_9_R2();
+        } else if (version.equalsIgnoreCase("v1_10_R1")) {
+            nms = new v1_10_R1();
+        } else if (version.equalsIgnoreCase("v1_11_R1")) {
+            nms = new v1_11_R1();
+        } else if (version.equalsIgnoreCase("v1_12_R1")) {
+            nms = new v1_12_R1();
+        } else {
+            getServer().getConsoleSender().sendMessage(Strings.red + "This plugin wil not work properly with version" + version);
+        }
+    }
 
 }
