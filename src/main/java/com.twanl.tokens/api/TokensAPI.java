da71a438d07c412eee40db47ae2c65999a2b634d
BuildTools <unconfigured@null.spigotmc.org>
2018-06-16 01:08:34 +0200

    updated the public API

--- src/main/java/com.twanl.tokens/api/TokensAPI.java
+++ src/main/java/com.twanl.tokens/api/TokensAPI.java
@@ -1,17 +1,12 @@
 package com.twanl.tokens.api;
 
-import com.twanl.tokens.utils.Functions;
 import com.twanl.tokens.Tokens;
 import com.twanl.tokens.utils.ConfigManager;
+import com.twanl.tokens.utils.Functions;
 import com.twanl.tokens.utils.Strings;
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.UUID;
 
 
@@ -19,221 +14,47 @@ import java.util.UUID;
  * Created by Twan on 3/22/2018.
  **/
 
-@SuppressWarnings({"UnusedReturnValue", "UnnecessaryLocalVariable"})
+
 public class TokensAPI {
 
     private static Tokens plugin = Tokens.getPlugin(Tokens.class);
     private ConfigManager cfgM = new ConfigManager();
     private Functions F = new Functions();
-    private Economy economy = Tokens.economy;
 
 
+    public void playerRemoveTokens(Player p, int tokens) {
 
-    public void addTokens (UUID targetUUID, UUID playerUUID, int tokens) {
-        int playerTokens = cfgM.getPlayers().getInt(targetUUID + ".tokens");
-
-        cfgM.getPlayers().set(targetUUID + ".tokens", playerTokens + tokens);
+        cfgM.getPlayers().set(p.getUniqueId() + ".tokens", playerBalance(p) - tokens);
         cfgM.savePlayers();
+    }
 
+    public void playerAddTokens(Player p, int tokens) {
+
+        cfgM.getPlayers().set(p.getUniqueId() + ".tokens", playerBalance(p) + tokens);
+        cfgM.savePlayers();
+    }
+
+    public void playerSetTokens(Player p, int tokens) {
+
+        cfgM.getPlayers().set(p.getUniqueId() + ".tokens", tokens);
+        cfgM.savePlayers();
+    }
+
+    public int playerBalance(Player p) {
+
+        return cfgM.getPlayers().getInt(p.getUniqueId() + ".tokens");
+    }
+
+
+    public boolean hasAccount(UUID playerUUID) {
         Player p = (Player) Bukkit.getOfflinePlayer(playerUUID);
-        p.sendMessage(Strings.green + tokens + " Tokens" + Strings.gray + " are added to " + Strings.green + F.getName(String.valueOf(targetUUID)));
-    }
 
-    public void removeTokens (UUID targetUUID, UUID playerUUID, int tokens) {
-
-        int playerTokens = cfgM.getPlayers().getInt(targetUUID + ".tokens");
-
-        cfgM.getPlayers().set(targetUUID + ".tokens", playerTokens - tokens);
-        cfgM.savePlayers();
-
-        Player p = (Player) Bukkit.getOfflinePlayer(playerUUID);
-        p.sendMessage(Strings.green + tokens + " Tokens " + Strings.gray + "are removed from " + Strings.green +  F.getName(String.valueOf(targetUUID)));
-    }
-
-    public void setTokens (UUID targetUUID, UUID playerUUID, int tokens) {
-
-        cfgM.getPlayers().set(targetUUID + ".tokens", tokens);
-        cfgM.savePlayers();
-
-        Player p = Bukkit.getPlayer(playerUUID);
-        p.sendMessage(Strings.green + tokens + " Tokens" + Strings.gray + " are set to " + Strings.green + F.getName(String.valueOf(targetUUID)));
-    }
-
-    public void giveallTokens (int tokens) {
-
-        String tokens1 = String.valueOf(tokens);
-        String defaultMessage = plugin.getConfig().getString("bonus_message");
-        String replacedMessage = defaultMessage.replace("{tokens}", tokens1);
-
-        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-            int playerTokens = cfgM.getPlayers().getInt(onlinePlayer.getUniqueId() + ".tokens");
-            cfgM.getPlayers().set(onlinePlayer.getUniqueId() + ".tokens", playerTokens + tokens);
-
-            Strings.translateColorCodes(onlinePlayer, replacedMessage);
-        }
-        cfgM.savePlayers();
-    }
-
-
-    public boolean convertToTokens (UUID playerUUID, int amount) {
-
-        Player p = Bukkit.getPlayer(playerUUID);
-        // getting some information
-        int tokensValue = plugin.getConfig().getInt("tokens.buy_price");
-        int totalCheckOut = amount * tokensValue;
-        Player buyer = p.getPlayer();
-        int playerTokens = cfgM.getPlayers().getInt(playerUUID + ".tokens");
-
-        // check if the player has enough balance to continue the transaction
-        if (totalCheckOut > economy.getBalance(buyer)) {
-            p.sendMessage(Strings.red + "You don't have enough money to buy Tokens!\n" +
-            " \n" +
-            Strings.gold + "Total costs: $" + totalCheckOut);
-            return true;
+        if (!cfgM.getPlayers().contains(String.valueOf(p.getUniqueId()))) {
+            return false;
         }
 
-
-        // ....
-        EconomyResponse r = economy.withdrawPlayer(buyer, totalCheckOut);
-        if (r.transactionSuccess()) {
-            cfgM.getPlayers().set(playerUUID + ".tokens", playerTokens + amount);
-            cfgM.savePlayers();
-            p.sendMessage(Strings.gray + "You bought " + Strings.green + amount + " Tokens " + Strings.gray + "for " + Strings.green + totalCheckOut);
-            return true;
-        } else {
-            p.sendMessage("An error accured!");
-            return true;
-        }
+        return true;
     }
-
-    public boolean convertToMoney (UUID playerUUID, int amount) {
-
-        Player p = Bukkit.getPlayer(playerUUID);
-        // getting some information
-        int tokensValue = plugin.getConfig().getInt("tokens.sell_price");
-        int playerTokens = cfgM.getPlayers().getInt(playerUUID + ".tokens");
-        int totalCheckOut = amount * tokensValue;
-        Player buyer = p.getPlayer();
-
-
-        // Check if player has enough tokens
-        if (amount > playerTokens) {
-            p.sendMessage(Strings.red + "You don't have that many tokens to sell!\n" +
-                    " \n" +
-                    Strings.gold + "Total Tokens: " + playerTokens);
-            return true;
-        }
-
-
-        // ....
-        EconomyResponse r = economy.depositPlayer(buyer, totalCheckOut);
-        if (r.transactionSuccess()) {
-
-            cfgM.getPlayers().set(playerUUID + ".tokens", playerTokens - amount);
-            cfgM.savePlayers();
-            p.sendMessage(Strings.gray + "You sold " + Strings.green + amount + " Tokens " + Strings.gray + "for " + Strings.green + totalCheckOut);
-
-            return true;
-        } else {
-            p.sendMessage("An error accured!");
-            return true;
-        }
-
-    }
-
-    public void balance (UUID playerUUID) {
-        int tokensBalance = cfgM.getPlayers().getInt(playerUUID + ".tokens");
-        Player p = Bukkit.getPlayer(playerUUID);
-        p.sendMessage(Strings.gray + "You have " + Strings.green + tokensBalance + " Tokens");
-    }
-
-
-    public void balance (UUID playerUUID, UUID targetUUID) {
-        // check if player exist before paying
-        if (!cfgM.getPlayers().contains(String.valueOf(targetUUID))) {
-            Player p = Bukkit.getPlayer(playerUUID);
-            p.sendMessage(Strings.red + "Player not found");
-        } else {
-            int tokensBalance = cfgM.getPlayers().getInt(targetUUID + ".tokens");
-            //Player p = Bukkit.getPlayer(playerUUID);
-            Player p = Bukkit.getPlayer(playerUUID);
-            p.sendMessage(Strings.gray + F.getName(targetUUID.toString()) + " has " + Strings.green + tokensBalance + " Tokens");
-        }
-    }
-
-
-
-    public void payPlayer(UUID playerUUID, UUID targetUUID, int tokens) {
-
-        // check if player exist before paying
-        if (!cfgM.getPlayers().contains(String.valueOf(targetUUID))) {
-            Player p = Bukkit.getPlayer(playerUUID);
-            p.sendMessage(Strings.red + "Player not found");
-        } else {
-
-            int targetTokens = cfgM.getPlayers().getInt(targetUUID + ".tokens");
-            int playerTokens = cfgM.getPlayers().getInt(playerUUID + ".tokens");
-
-            cfgM.getPlayers().set(playerUUID + ".tokens", playerTokens - tokens);
-            cfgM.getPlayers().set(targetUUID + ".tokens", tokens + targetTokens);
-            cfgM.savePlayers();
-
-            Player p = (Player) Bukkit.getOfflinePlayer(playerUUID);
-            p.sendMessage(Strings.gray + "You payed " + Strings.green + F.getName(String.valueOf(targetUUID)) + " " + tokens);
-
-            OfflinePlayer playerReceiver = Bukkit.getOfflinePlayer(F.getName(String.valueOf(targetUUID)));
-            if (playerReceiver.isOnline()) {
-                Player pOnline = Bukkit.getPlayer(playerReceiver.getUniqueId());
-                pOnline.sendMessage(Strings.gray + "You received " + Strings.green + tokens + " Tokens " + Strings.gray + "from " + Strings.green + p.getName());
-            }
-
-
-            Object transactionTime_HMS = new SimpleDateFormat("HH:mm:ss").format(new Date());
-            Object transactionTime_YMD = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-
-            cfgM.getPlayers().set(targetUUID + ".LastTransactionActivity.date", transactionTime_HMS + " | " + transactionTime_YMD);
-            cfgM.getPlayers().set(targetUUID + ".LastTransactionActivity.player", F.getName(playerUUID.toString()));
-            cfgM.getPlayers().set(targetUUID + ".LastTransactionActivity.amount", tokens);
-            cfgM.savePlayers();
-
-
-        }
-    }
-
-
-    // add player to database if player not exist
-    public void creatAccount(UUID uuid) {
-        if (!cfgM.getPlayers().contains(String.valueOf(uuid))) {
-            cfgM.getPlayers().set(uuid + ".tokens", 0);
-            cfgM.savePlayers();
-        }
-    }
-
-
-    // Value of the balance of the player :: INT
-    public int balanceInt (UUID playerUUID) {
-        int playerTokens = cfgM.getPlayers().getInt(playerUUID + ".tokens");
-        return playerTokens;
-    }
-
-
-    public void transactionSuccess(UUID uuid, UUID targetUUID, int tokens) {
-        Player p = Bukkit.getPlayer(uuid);
-        int targetPBalance_before = balanceInt(targetUUID);
-
-
-        payPlayer(uuid, targetUUID, tokens);
-
-        int targetPBalance_after = balanceInt(targetUUID);
-
-        if (targetPBalance_before == targetPBalance_after) {
-            p.sendMessage(Strings.redI + "failed to send the tokens!");
-        } else {
-            // send nothing when transaction worked
-        }
-
-    }
-
 
 
 }
