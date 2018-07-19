4cc79693230670bd6f72798fc410e4d344e94287
BuildTools <unconfigured@null.spigotmc.org>
2018-07-19 21:54:25 +0200

    you need to change only the resource value, for faster code

--- src/main/java/com.twanl.tokens/utils/UpdateChecker.java
+++ src/main/java/com.twanl.tokens/utils/UpdateChecker.java
@@ -25,17 +25,18 @@ public class UpdateChecker {
     public String getLatestVersion() {
         try {
             int resource = 53944;
-            HttpURLConnection con = (HttpURLConnection)new URL("https://api.spigotmc.org/legacy/update.php?resource=53944").openConnection();
+            HttpURLConnection con = (HttpURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=" + resource).openConnection();
             con.setDoOutput(true);
             con.setRequestMethod("POST");
-            con.getOutputStream().write("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=53944".getBytes("UTF-8"));
+            String key = "key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=" + resource;
+            con.getOutputStream().write(key.getBytes("UTF-8"));
             String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
             if (version.length() <= 7) {
                 return version;
             }
         } catch (Exception ex) {
             System.out.println("---------------------------------");
-            this.plugin.getLogger().info("Failed to check for a update on spigot.");
+            plugin.getLogger().info("Failed to check for a update on spigot.");
             System.out.println("---------------------------------");
         }
         return null;
