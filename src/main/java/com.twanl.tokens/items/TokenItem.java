d34beeb70425709bf6896b6d80166bd738c50f78
Twan Luttik <twanluttik@gmail.com>
2018-07-29 05:31:36 +0200

    REWORKED, changed from a placeable item to a non-placeable item, and can handle stacked items

--- src/main/java/com.twanl.tokens/items/TokenItem.java
+++ src/main/java/com.twanl.tokens/items/TokenItem.java
@@ -1,94 +1,100 @@
 package com.twanl.tokens.items;
 
 import com.twanl.tokens.lib.Lib;
+import com.twanl.tokens.sql.SQLlib;
 import com.twanl.tokens.utils.ConfigManager;
 import com.twanl.tokens.utils.Strings;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
-import java.util.Random;
 
-import static org.bukkit.Material.DOUBLE_PLANT;
 
 /**
  * Created by Twan on 3/22/2018.
  **/
 
-public class TokenItem implements Listener{
+public class TokenItem implements Listener {
 
     private ConfigManager cfgM = new ConfigManager();
     private Lib lib = new Lib();
+    private SQLlib sql = new SQLlib();
 
-    public void addToken (Player p, int tokens) {
 
-        // get a random number to prevent stacking items
-        Random rand = new Random();
-        int n = rand.nextInt(1000000) + 1;
+    public void addTokenNote(Player p, int amount) {
 
-        // CODE of the custom item
-        ItemStack item = new ItemStack(Material.DOUBLE_PLANT, 1);
+        // The custom item
+        ItemStack item = new ItemStack(Material.PAPER, 1);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(Strings.gold + tokens +  Strings.gray + " " + lib.getPrefix());
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add(Strings.gold + "/tokens redeem " + Strings.gray + "to redeem the " + lib.getPrefix());
+        //noinspection RedundantArrayCreation
+        meta.addItemFlags(new ItemFlag[]{ItemFlag.HIDE_ATTRIBUTES});
+        meta.setDisplayName(Strings.green + "Note");
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(Strings.white + amount + " " + lib.getPrefix());
         lore.add(" ");
-        lore.add(Strings.grayI + "ID: " + String.valueOf(n));
+        lore.add(Strings.gray + "Owner: " + p.getName());
         meta.setLore(lore);
         item.setItemMeta(meta);
 
+        // add the item to the player
         p.getInventory().addItem(item);
 
+        // remove the tokens from the player
+        if (lib.sqlUse()) {
+            sql.removeTokens(p.getUniqueId(), amount);
+        } else {
+            cfgM.getPlayers().set(p.getUniqueId() + ".tokens", lib.balanceInt(p.getUniqueId()) - amount);
+            cfgM.savePlayers();
+        }
+        p.sendMessage(Strings.green + amount + " " + lib.getPrefix() + " " + Strings.gray + "are converted to your inventory");
 
-        int playerTokens = cfgM.getPlayers().getInt(p.getUniqueId() + ".tokens");
-        p.sendMessage(Strings.green + tokens + " " + lib.getPrefix() + " " + Strings.gray + "are converted to your inventory");
-
-        cfgM.getPlayers().set(p.getUniqueId() + ".tokens", playerTokens - tokens);
-        cfgM.savePlayers();
     }
 
 
-    //TODO: is not removing the item from the player
-    @SuppressWarnings("deprecation")
-    public void removeToken(Player p) {
-
-        // get the int from the itemlore(Custom item) specific line
+    public void removeTokenNote(Player p) {
         String[] getLoreList = Strings.stripColor(p.getItemInHand().getItemMeta().getLore().toString()).split(" ");
 
+        String a = getLoreList[0];
+        String amount = a.replace("[", "");
 
-        String getLoreLine = getLoreList[9];
-        String getLoreId = getLoreLine.replaceAll("]", "");
+        String b = getLoreList[6];
+        String ownerName = b.replace("]", "");
 
 
-        // get the first word(int in this case) and convert to to a string without any color codes
-        String[] tokenItem = ChatColor.stripColor(p.getItemInHand().getItemMeta().getDisplayName()).split(" ");
-        String firstword = tokenItem[0];
-
-        // CODE for getting the item
-        ItemStack item = new ItemStack(DOUBLE_PLANT, 1);
+        // the custom item with the dynamic lore
+        ItemStack item = new ItemStack(Material.PAPER, 1);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(Strings.gold + firstword +  Strings.gray + " " + lib.getPrefix());
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add(Strings.gold + "/tokens redeem " + Strings.gray + "to redeem the " + lib.getPrefix());
+        //noinspection RedundantArrayCreation
+        meta.addItemFlags(new ItemFlag[]{ItemFlag.HIDE_ATTRIBUTES});
+        meta.setDisplayName(Strings.green + "Note");
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(Strings.white + amount + " " + lib.getPrefix());
         lore.add(" ");
-        lore.add(Strings.grayI + "ID: " + getLoreId);
+        lore.add(Strings.gray + "Owner: " + ownerName);
         meta.setLore(lore);
         item.setItemMeta(meta);
 
 
-        int tokensAmount = Integer.parseInt(firstword);
-        int playerTokens = cfgM.getPlayers().getInt(p.getUniqueId() + ".tokens");
-        p.sendMessage(Strings.gray + "You converted " + Strings.green + tokensAmount + " " + lib.getPrefix() + " " + Strings.gray + " to your account");
+        //noinspection deprecation
+        p.getItemInHand().setAmount(p.getItemInHand().getAmount() - 1);
 
-        cfgM.getPlayers().set(p.getUniqueId() + ".tokens", playerTokens + tokensAmount);
-        cfgM.savePlayers();
 
-        p.getInventory().remove(item);
+        if (lib.sqlUse()) {
+            sql.addTokens(p.getUniqueId(), Integer.parseInt(amount));
+        } else {
+            int tokensAmount = Integer.parseInt(amount);
+            int playerTokens = cfgM.getPlayers().getInt(p.getUniqueId() + ".tokens");
+
+            cfgM.getPlayers().set(p.getUniqueId() + ".tokens", playerTokens + tokensAmount);
+            cfgM.savePlayers();
+        }
+        p.sendMessage(Strings.gray + "You converted " + Strings.green + amount + " " + lib.getPrefix() + Strings.gray + " to your account");
 
     }
+
 
 }
