944f11051df8d143f6d887d962901e270140310b
Twan Luttik <twanluttik@gmail.com>
2018-08-23 21:20:50 +0200

    added a sell methode and it won't for loop anymore it checking what slot the player clicked on

--- src/main/java/com.twanl.tokens/menu/ShopMenu.java
+++ src/main/java/com.twanl.tokens/menu/ShopMenu.java
@@ -11,10 +11,13 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
 
 
 /**
@@ -71,12 +74,20 @@ public class ShopMenu implements Listener {
 
 
                 // for loop, for the items
-                for (String i : config.getShop().getConfigurationSection("shop." + SHOP_NAME + ".slots").getKeys(false)) {
-                    int SLOT_NUMBER = Integer.parseInt(i);
+//                for (String i : config.getShop().getConfigurationSection("shop." + SHOP_NAME + ".slots").getKeys(false)) {
+//                    int SLOT_NUMBER = Integer.parseInt(i);
+                    int SLOT_NUMBER = a;
 
 
-                    String colorText = Strings.translateColorCodes(lib.itemName(SHOP_NAME, SLOT_NUMBER));
+                    String colorText = Strings.translateColorCodes(lib.itemName(SHOP_NAME, a));
                     if (item.getItemMeta().getDisplayName().equals(colorText)) {
+
+                        int itemID = lib.itemId(SHOP_NAME, a);
+                        int itemByte = lib.itemByte(SHOP_NAME, a);
+                        int itemAmount = lib.itemAmount(SHOP_NAME, a);
+                        int itemPrice = lib.itemSellPrice(SHOP_NAME, a);
+                        int playerBalance = lib.balanceInt(p.getUniqueId());
+                        String itemName = Strings.translateColorCodes(lib.itemName(SHOP_NAME, a));
 
 
                         // checks if the palyer has permission
@@ -86,6 +97,7 @@ public class ShopMenu implements Listener {
                                 p.closeInventory();
                                 return;
                             }
+                            return;
                         }
 
                         // close the shop
@@ -103,98 +115,84 @@ public class ShopMenu implements Listener {
                             return;
                         }
 
-//                            if (e.getClick() == ClickType.LEFT) {
-//
-//                                p.sendMessage("BUYING 1 ITEM");
-//                            }
-//
-//                            if (e.getClick() == ClickType.RIGHT) {
-//
-//                                p.sendMessage("SELLING 1 ITEM");
-//                            }
-
-                        // some base information
-                        int itemPrice = lib.itemPrice(SHOP_NAME, SLOT_NUMBER);
-                        int playerBalance = lib.balanceInt(p.getUniqueId());
+                        // Buy methode
+                        if (e.getClick() == ClickType.LEFT) {
 
 
-                        if (itemPrice > playerBalance) { // check if the player has enough tokens
-                            p.closeInventory();
-                            p.updateInventory(); // it will prevent from shift clicking the item to the inventory
-                            p.sendMessage(Strings.red + "You don't have enought to buy this!");
-                            return;
-                        } else { // the payment methode
+
+                            if (itemPrice > playerBalance) { // check if the player has enough tokens
+                                p.closeInventory();
+                                p.updateInventory(); // it will prevent from shift clicking the item to the inventory
+                                p.sendMessage(Strings.red + "You don't have enought to buy this!");
+                                return;
+                            } else { // the payment methode
 
 
-                            // so you don't need t write a command to give the player the iten with the amount (just for faster configuring)
-                            if (lib.itemCommand(SHOP_NAME, SLOT_NUMBER).contains("<item>")) {
-                                if (p.getInventory().firstEmpty() == -1) {
-                                    p.closeInventory();
-                                    p.updateInventory();
-                                    p.sendMessage(Strings.red + "You don't have enough inventory space");
+                                // so you don't need t write a command to give the player the iten with the amount (just for faster configuring)
+                                if (lib.itemCommand(SHOP_NAME, SLOT_NUMBER).contains("<item>")) {
+                                    if (p.getInventory().firstEmpty() == -1) {
+                                        p.closeInventory();
+                                        p.updateInventory();
+                                        p.sendMessage(Strings.red + "You don't have enough inventory space");
+                                        return;
+                                    }
+
+                                    ItemStack item1 = new ItemStack(Material.getMaterial(itemID), itemAmount, (short) itemByte);
+
+                                    p.getInventory().addItem(item1);
+                                    lib.removeTokens(null, p.getUniqueId(), itemPrice);
+                                    p.updateInventory(); // it will prevent from shift clicking the item to the inventory
+                                    p.sendMessage(Strings.gray + "You bought " + Strings.green + itemAmount + "X " + Strings.gray + itemName + " for " + Strings.green + itemPrice + " " + Strings.white + lib.getPrefix());
                                     return;
                                 }
 
-                                int itemID = lib.itemId(SHOP_NAME, SLOT_NUMBER);
-                                int itemByte = lib.itemByte(SHOP_NAME, SLOT_NUMBER);
-                                int itemAmount = lib.itemAmount(SHOP_NAME, a);
-                                ItemStack item1 = new ItemStack(Material.getMaterial(itemID), itemAmount, (short) itemByte);
+                                // for executing a custom command
+                                if (lib.itemCommand(SHOP_NAME, SLOT_NUMBER).contains("<command>")) {
+                                    String command = lib.itemCommand(SHOP_NAME, SLOT_NUMBER).replace("<command>", "");
+                                    String command1 = command.replace("{player}", p.getName());
 
-//                                    p.closeInventory();
-                                p.getInventory().addItem(item1);
-                                lib.removeTokens(null, p.getUniqueId(), itemPrice);
-                                p.updateInventory(); // it will prevent from shift clicking the item to the inventory
-                                p.sendMessage(Strings.gray + "You have got " + Strings.green + itemAmount + "X " + Strings.translateColorCodes(lib.itemName(SHOP_NAME, SLOT_NUMBER)));
-                                return;
+                                    p.closeInventory();
+                                    lib.removeTokens(null, p.getUniqueId(), itemPrice);
+                                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command1);
+                                    return;
+                                }
                             }
-
-                            // for executing a custom command
-                            if (lib.itemCommand(SHOP_NAME, SLOT_NUMBER).contains("<command>")) {
-                                String command = lib.itemCommand(SHOP_NAME, SLOT_NUMBER).replace("<command>", "");
-                                String command1 = command.replace("{player}", p.getName());
-
-                                p.closeInventory();
-                                lib.removeTokens(null, p.getUniqueId(), itemPrice);
-                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command1);
-                                return;
-                            }
+                            return;
                         }
 
+                        // Sell methode
+                        if (e.getClick() == ClickType.RIGHT) {
 
+                            ItemStack item1 = new ItemStack(itemID, 1, (short) itemByte);
+                            if (p.getInventory().containsAtLeast(item1, 1)) {
+
+                                // counting the total item of the item
+                                int itemCount = 0;
+                                for (ItemStack iStack : p.getInventory().getContents()) {
+                                    if (iStack != null && iStack.getType() == item1.getType()) {
+                                        itemCount = itemCount + iStack.getAmount();
+                                    }
+                                }
+
+                                ItemStack itemRemove = new ItemStack(itemID, itemAmount, (short) itemByte);
+
+                                if (itemCount >= itemAmount) {
+                                    lib.addTokens(null, p.getUniqueId(), itemPrice);
+                                    p.sendMessage(Strings.gray + "You sold " + Strings.green + itemAmount + "X " + Strings.gray + itemName + " for " + Strings.green + itemPrice + " " + Strings.white + lib.getPrefix());
+                                    p.getInventory().removeItem(itemRemove);
+                                    p.updateInventory();
+                                }
+
+
+                                return;
+                            }
+                            return;
+                        }
                     }
-
+                    return;
                 }
-
-
             }
-
         }
-
-
-    }
-
-//    @EventHandler
-//    public void test(InventoryClickEvent e) {
-//        Player p = (Player) e.getWhoClicked();
-//        Inventory open = e.getInventory();
-//        ItemStack item = e.getCurrentItem();
-//        config.setup();
-//
-//        int a = e.getSlot();
-//
-//
-//        if (open == null) {
-//            return;
-//        }
-//
-//
-//        if (util.editMode.get(p)) {
-//            if(e.getCurrentItem() == null) {
-//                p.sendMessage("test");
-//                return;
-//            }
-//        }
-//
 //    }
 
     private boolean B = false;
@@ -215,7 +213,9 @@ public class ShopMenu implements Listener {
             if (e.getInventory().getName().equals(colorTitle + Strings.DgrayB + " - " + Strings.redB + "EDIT MODE")) {
 
 
-                if (B) {Bukkit.getConsoleSender().sendMessage("----------------------------");}
+                if (B) {
+                    Bukkit.getConsoleSender().sendMessage("----------------------------");
+                }
                 for (int i = 0; i < e.getInventory().getSize(); i++) {
 
                     // check if the slot is empty than clear that in the file
@@ -223,16 +223,22 @@ public class ShopMenu implements Listener {
                         if (config.getShop().isSet("shop." + menu + ".slots." + i)) {
                             config.getShop().set("shop." + menu + ".slots." + i, null);
                             config.saveShop();
-                            if (B){Bukkit.getConsoleSender().sendMessage(Strings.red + "SLOT: "+ i + " REMOVED FROM FILE");}
+                            if (B) {
+                                Bukkit.getConsoleSender().sendMessage(Strings.red + "SLOT: " + i + " REMOVED FROM FILE");
+                            }
                         } else {
-                            if (B) {Bukkit.getConsoleSender().sendMessage(Strings.blue + "SLOT: " + i + " NULL");}
+                            if (B) {
+                                Bukkit.getConsoleSender().sendMessage(Strings.blue + "SLOT: " + i + " NULL");
+                            }
                         }
 
 
                     } else if (e.getInventory().getItem(i) != null) {
                         // if the slot has a item in, check if the item is the same as in the config file for preventing removing edited data from the file else remove the data from the file and put new data into the file
                         if (e.getInventory().getItem(i).getType().getId() == lib.itemId(menu, i) && e.getInventory().getItem(i).getDurability() == lib.itemByte(menu, i)) {
-                            if (B) {Bukkit.getConsoleSender().sendMessage("SLOT: " + i + " ITEM IS THE SAME AS IN FILE!");}
+                            if (B) {
+                                Bukkit.getConsoleSender().sendMessage("SLOT: " + i + " ITEM IS THE SAME AS IN FILE!");
+                            }
                         } else {
                             int amount1 = e.getInventory().getItem(i).getAmount();
                             int itemID = e.getInventory().getItem(i).getType().getId();
@@ -245,10 +251,13 @@ public class ShopMenu implements Listener {
                             config.getShop().set("shop." + menu + ".slots." + i + ".amount", amount1);
                             config.getShop().set("shop." + menu + ".slots." + i + ".Id", itemID);
                             config.getShop().set("shop." + menu + ".slots." + i + ".Data", itemByte);
-                            config.getShop().set("shop." + menu + ".slots." + i + ".cost", 0);
+                            config.getShop().set("shop." + menu + ".slots." + i + ".buy", 0);
+                            config.getShop().set("shop." + menu + ".slots." + i + ".sell", 0);
                             config.getShop().set("shop." + menu + ".slots." + i + ".command", "<item>");
 
-                            if (B) {Bukkit.getConsoleSender().sendMessage(Strings.green + "SLOT: " + i + " ITEM HAS BEEN ADDED");}
+                            if (B) {
+                                Bukkit.getConsoleSender().sendMessage(Strings.green + "SLOT: " + i + " ITEM HAS BEEN ADDED");
+                            }
                             config.saveShop();
                         }
 
@@ -257,7 +266,9 @@ public class ShopMenu implements Listener {
                 }
 
                 config.saveShop();
-                if (B) {Bukkit.getConsoleSender().sendMessage("----------------------------");}
+                if (B) {
+                    Bukkit.getConsoleSender().sendMessage("----------------------------");
+                }
                 p.sendMessage(Strings.prefix + Strings.green + "inventory saved!");
                 util.editMode.put(p, false);
 
@@ -287,7 +298,14 @@ public class ShopMenu implements Listener {
             // if the itemname has color codes than translate it to colored text
             String itemNameColor = Strings.translateColorCodes(lib.itemName(menu, i1));
 
-            inv.addItem(i, itemNameColor, lib.itemAmount(menu, i1), lib.itemSlot(menu, i1), lib.itemByte(menu, i1), Material.getMaterial(lib.itemId(menu, i1)), " ", Strings.gray + "Price: " + Strings.green + lib.itemPrice(menu, i1) + " " + lib.getPrefix());
+//            inv.addItem(i, itemNameColor, lib.itemAmount(menu, i1), lib.itemSlot(menu, i1), lib.itemByte(menu, i1), Material.getMaterial(lib.itemId(menu, i1)), " ", Strings.gray + "Price: " + Strings.green + lib.itemPrice(menu, i1) + " " + lib.getPrefix());
+
+            ArrayList<String> info = new ArrayList<>();
+            info.add(" ");
+            info.add(Strings.gray + "Buy: " + Strings.green + lib.itemBuyPrice(menu, i1));
+            info.add(Strings.gray + "Sell: " + Strings.green + lib.itemSellPrice(menu, i1));
+
+            inv.addItem(i, itemNameColor, lib.itemAmount(menu, i1), lib.itemSlot(menu, i1), lib.itemByte(menu, i1), Material.getMaterial(lib.itemId(menu, i1)), info);
         }
 
         p.openInventory(i);
@@ -312,7 +330,13 @@ public class ShopMenu implements Listener {
             // if the itemname has color codes than translate it to colored text
             String itemNameColor = Strings.translateColorCodes(lib.itemName(menu, i1));
 
-            inv.addItem(i, itemNameColor, lib.itemAmount(menu, i1), lib.itemSlot(menu, i1), lib.itemByte(menu, i1), Material.getMaterial(lib.itemId(menu, i1)), " ", Strings.gray + "Price: " + Strings.green + lib.itemPrice(menu, i1) + " " + lib.getPrefix());
+            //inv.addItem(i, itemNameColor, lib.itemAmount(menu, i1), lib.itemSlot(menu, i1), lib.itemByte(menu, i1), Material.getMaterial(lib.itemId(menu, i1)), " ", Strings.gray + "Price: " + Strings.green + lib.itemPrice(menu, i1) + " " + lib.getPrefix());
+            ArrayList<String> info = new ArrayList<>();
+            info.add(" ");
+            info.add(Strings.gray + "Buy: " + Strings.green + lib.itemBuyPrice(menu, i1));
+            info.add(Strings.gray + "Sell: " + Strings.green + lib.itemSellPrice(menu, i1));
+
+            inv.addItem(i, itemNameColor, lib.itemAmount(menu, i1), lib.itemSlot(menu, i1), lib.itemByte(menu, i1), Material.getMaterial(lib.itemId(menu, i1)), info);
         }
 
         p.openInventory(i);
